c63c5bdc0c581fcc4c2b3e58c1c48e5db8e6a791
Eshaan <eshaannair3456@gmail.com>
2026-01-24 15:35:46 +0530

    updated vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app=express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://smart-expense-splitter-hhpi.vercel.app'
+    'https://smart-expense-splitter-mern.vercel.app'
   ],
   credentials: true
 }));
