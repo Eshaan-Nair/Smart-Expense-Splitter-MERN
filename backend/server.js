96fec32ccdf52f88910541f7cf0f2d9944ebed6b
Eshaan <eshaannair3456@gmail.com>
2026-01-26 04:46:38 +0530

    Vercel Link update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app=express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://smart-expense-splitter-mern.vercel.app'
+    'https://splitsmart-mern.vercel.app'
   ],
   credentials: true
 }));
